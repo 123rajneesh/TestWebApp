f3effb8e986df7ee25375285ede90abe13fba133
Rajneesh.Kumar <Rajneesh.Kumar@HSSLPT-113>
2018-03-25 20:10:29 +0530

    Add project files.

---
+++ TestASPWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestASPWeb.Startup))]
+namespace TestASPWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
